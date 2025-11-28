92fbcd1825c0bd7c4fbf9bfa4e3daf26ec241b00
joat000 <harjot0665@gmail.com>
2025-11-28 01:20:43 -0600

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -904,7 +904,7 @@ function startChat(friendId, friendName) {
         loadMessages();
 
         if (chatPollInterval) clearInterval(chatPollInterval);
-        chatPollInterval = setInterval(loadMessages, 3000);
+        chatPollInterval = setInterval(loadMessages, 1000);
     }
 }
 
